aa22383f4b94477f26aef03a63ecfcf921e2817c
shivisingh28 <shivi2819997@gmail.com>
2020-10-24 13:13:02 +0530

    backend api done

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+require("dotenv").config();
+const logs = require("./api/logs");
 const middlewares = require("./middlewares");
 
-
-
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
 app.use(morgan("common"));
 app.use(helmet());
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: process.env.CORS_ORIGIN,
 	})
 );
 
@@ -21,6 +24,8 @@ app.get("/", (req, res) => {
 		message: "Hello Peeps !!",
 	});
 });
+
+app.use("/api/logs", logs);
 
 app.use(middlewares.notFound);
 
